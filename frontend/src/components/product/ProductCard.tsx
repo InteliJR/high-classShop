f9f7ebeb22aacfd2433066f5338dcde93b79a1da
anacajp <159061342+Anacajp@users.noreply.github.com>
2025-10-19 12:28:01 -0300

    feat/adiciona parte esquerda da página com layout desatualizado

--- frontend/src/components/product/ProductCard.tsx
+++ frontend/src/components/product/ProductCard.tsx
@@ -1,31 +1,31 @@
-interface CardProdutoProps {
+interface CardProductProps {
     image_url: string,
-    marca: string,
-    modelo: string,
-    descricao: string,
-    valor: number
+    brand: string,
+    model: string,
+    description: string,
+    value: number
 }
 
-function formatValue(valor : number){
-    return valor.toLocaleString('pt-br', {style: 'currency', currency:'BRL'})
+function formatValue(value : number){
+    return value.toLocaleString('pt-br', {style: 'currency', currency:'BRL'})
 }
 
-export default function CardProduto( { image_url, marca, modelo, descricao, valor} : CardProdutoProps){
+export default function CardProduct( { image_url, brand, model, description, value} : CardProductProps){
     return(
         <article className="h-full w-full rounded-lg border border-black px-6 pt-6 pb-4 flex flex-col gap-2">
             <div className="flex justify-center">
-                <img src={image_url} alt={`Imagem do produto ${marca} ${modelo}`} className="rounded-lg"/>
+                <img src={image_url} alt={`Imagem do produto ${brand} ${model}`} className="rounded-lg"/>
             </div>
 
             <div className="flex flex-col text-base gap-2 justify-center items-center">
                 <h2 className="font-semibold">
-                    {marca} {modelo}
+                    {brand} {model}
                 </h2>
                 <p className="text-[8px] text-justify line-clamp-2">
-                    {descricao}
+                    {description}
                 </p>
                 <p className="">
-                    {formatValue(valor)}
+                    {formatValue(value)}
                 </p>
             </div>
         </article>
--- frontend/src/pages/SchedulerPage.tsx
+++ frontend/src/pages/SchedulerPage.tsx
@@ -1,13 +1,17 @@
-import ProductInfo from '../components/product/ProductInfo';
+import ProductDetails from '../components/product/ProductDetails';
 import Breadcrumb from '../components/ui/Breadcrumb';
 
-// import BookingCalendar from '../components/booking/BookingCalendar';
+//  import { useParams } from 'react-router-dom';
+//  import { useEffect, useState } from 'react';
+//  import ProductCard from '../components/product/ProductCard';
+//  import BookingCalendar from '../components/booking/BookingCalendar';
 
 
 const SchedulerPage = () => {
   // const { id } = useParams<{ id: string }>();
   // adicionar a linha de cima quando integrar com a api, por enquanto deixa o mock abaixo
 
+  
   const productData = {
     model: 'Toyota Corolla',
     year: 2023,
@@ -27,9 +31,9 @@ const SchedulerPage = () => {
       <Breadcrumb itemName={productData.model} />
 
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
-        <ProductInfo car={productData} />
+        <ProductDetails {...productData} />
 
-        {/* <BookingCalendar onSchedule={handleSchedule} /> */}
+        {/* // <BookingCalendar onSchedule={handleSchedule} /> */}
       </div>
     </div>
   );
