ac8f49f67af2e7104fe4c15d2a4334c5d32f94a0
Davi Duarte <rio.daviduarte@gmail.com>
2025-10-30 22:23:12 -0300

    feat: módulo do assessor integrado com AWS SES #62 #63 #64
    Adicionado módulo de assessor com controlador, serviço, DTOs e entidade cliente. Além disso, integração com o AWS SES para envio de e-mails, e utility para validação de e-mails. Adicionei um .env.example para ajudar na configuração.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompaniesModule } from './companies/companies.module';
-
+import { ConsultantModule } from './consultant/consultant.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [CompaniesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CompaniesModule,
+    ConsultantModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
