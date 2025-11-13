6521188e7a4d3b4ba40f40729f8ec4706477a20d
Messias-Olivindo <messias.olivindo@sou.inteli.edu.br>
2025-11-13 19:09:06 -0300

    feat: adiciona a sidebar para a versão mobile com a lógica de deixar ela visível com o botão do header

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
+import { AppProvider } from "../contexts/AppContext";
 
 interface MainLayoutProps {
   children: React.ReactNode;
@@ -8,12 +10,13 @@ interface MainLayoutProps {
 export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <>
-      <Header />
+      <AppProvider>
+        <Header />
+        <Sidebar />
+      </AppProvider>
       <div className="flex justify-center">
         {/* <aside className="w-31 bg-black h-screen"></aside> */}
-        <main className="flex flex-col flex-1 mx-34 my-8">
-          {children}
-        </main>
+        <main className="flex flex-col flex-1 mx-34 my-8">{children}</main>
       </div>
     </>
   );
