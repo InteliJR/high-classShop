943f677f4b082a4fe2553fc60046ea9add8867a6
Messias-Olivindo <messias.olivindo@sou.inteli.edu.br>
2025-11-19 15:37:37 -0300

    fix: conserta o erro de criação de consultant em que o password_hash que não guardava a senha de maneira segura com o hash

--- backend/src/features/consultants/consultants.service.ts
+++ backend/src/features/consultants/consultants.service.ts
@@ -1,7 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { CreateConsultantDto } from './dto/create-consultant.dto';
 import { UpdateConsultantDto } from './dto/update-consultant.dto';
+import { UserRole } from 'src/auth/dto/auth';
+import * as bcrypt from 'bcrypt';
+import { UserEntity } from 'src/auth/entities/user.entity';
+
 
 @Injectable()
 export class ConsultantsService {
@@ -16,12 +20,36 @@ export class ConsultantsService {
 
   // Cria um novo consultor na base de dados.
   async create(data: CreateConsultantDto) {
-    return this.prisma.user.create({
-      data: {
-        ...data,
-        role: 'CONSULTANT',
+    // Verificar se o usuário já existe
+    console.log("Na função de criar o consultor");
+    const userAlreadyExists = await this.prisma.user.findUnique({
+      where: {
+        email: data.email,   
       },
     });
+    if (userAlreadyExists) {
+      throw new UnauthorizedException('This user already exists');
+    }
+
+    // Criar o role para registrar o usuário padrão
+    const registerRole = UserRole.CONSULTANT;
+
+    // Separação da req
+    const { password, ...dataSave } = data;
+
+    // Criar o hash da senha
+    const passwordHash = await bcrypt.hash(data.password, 10);
+
+    // Cria o usuário
+    const user = await this.prisma.user.create({
+      data: { ...dataSave, password_hash: passwordHash, role: registerRole },
+    });
+
+    console.log(user);
+
+    return {
+      user: UserEntity.fromPrisma(user),
+    };
   }
 
   // Retorna um único consultor pelo ID.
